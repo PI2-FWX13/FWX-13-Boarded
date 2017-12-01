843e102160f7477671151acb20c67f9a385c8c8e
natmendes <nataliammendes04@gmail.com>
2017-11-30 22:12:43 -0200

    Codigo arrumado para teste na sexta.

---
+++ CodigoPARATESTARSEXTA/serial.h
@@ -0,0 +1,19 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+// Libraries
+#include <stdlib.h>
+#include <signal.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+// Definitions
+#define TTY "/dev/ttyS0"
+
+// Prototypes
+int serial_init(void);
+void serial_close(void);
+void serial_write(char *data, int size);
+char serial_read();
+
+#endif // SERIAL_H
